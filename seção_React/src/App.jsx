8e19108bb86114f8a346d4464a61e84baf874368
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-01-16 14:42:27 -0300

    Finalizando primeira seção

--- seção_React/src/App.jsx
+++ seção_React/src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
 
+  const [resul, setResul] = useState('Montou')
+
+  useState(() => {
+
+    return () => console.log('desmontou')
+
+  }, [])
 
   return (
     <>
-
+      <h1>Bem vindo</h1>
+      <h2>{resul}</h2>
     </>
   )
 }
