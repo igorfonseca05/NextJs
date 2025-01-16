a1d96a1ceb23e76124e681aebdc459cf91558131
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-01-16 08:39:01 -0300

    initial commit

--- seção_React/src/App.jsx
+++ seção_React/src/App.jsx
@@ -1,15 +1,10 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
-  const [resul, setResul] = useState('Montou')
-
-  useState(() => {
-
-    return () => console.log('desmontou')
-
-  }, [])
+  const [count, setCount] = useState(0)
 
   return (
     <>
