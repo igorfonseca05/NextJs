277ebc15882919282d598f74ed0fa1d3c8845ac4
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-01-17 11:19:30 -0300

    Meu primeiro projeto nextjs

---
+++ my_project/src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+import styles from './styles.module.css'
+
+export default function Header () {
+  return (
+    <header className={styles.header}>
+    <section className={styles.content}>
+        <nav className={styles.nav}>
+            <Link href={'/'}>
+            <h1 className={styles.logo}>Tarefa <span>+</span></h1>
+            </Link>
+            <Link href={'/dashboard'} className={styles.link}>
+                  Meu Painel
+            </Link>
+        </nav>
+        <button className={styles.loginButton}>Acessar</button>
+    </section>
+</header>
+  );
+};
--- my_project/src/pages/_app.tsx
+++ my_project/src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+// import Header from "../components/header/Header";
+
+import Header from "../components/Header/Header";
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return( 
+  <>
+  <Header/>
+  <Component {...pageProps} />
+  
+  </>
+  );
+  
 }
