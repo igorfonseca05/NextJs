db7f0d92af255f80d6c1dfd6af3bef7e2d2656be
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-01-20 09:26:33 -0300

    implementando OAuth authentication

--- my_project/src/pages/api/auth/[...nextAuth].ts
+++ my_project/src/pages/api/auth/[...nextAuth].ts
@@ -4,6 +4,7 @@ import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from 'next-auth/providers/google'
 
 export const authOptions = {
+<<<<<<< HEAD
     providers: [
        GoogleProvider( {
         clientId: process.env.GOOGLE_CLIENT_ID as string,
@@ -11,6 +12,16 @@ export const authOptions = {
        })
     ],
     secret: process.env.JWT_SECRET as string
+=======
+    // Configure one or more authentication providers
+    providers: [
+       GoogleProvider( {
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET
+       })
+        // ...add more providers here
+    ],
+>>>>>>> 147a31f (implementando OAuth authentication)
 }
 
 export default NextAuth(authOptions)
