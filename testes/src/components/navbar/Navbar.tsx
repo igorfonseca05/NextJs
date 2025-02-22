ebc87c8210b54831d897352647d5afd59d469614
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-02-22 07:19:32 -0300

    testing features

--- testes/src/components/navbar/Navbar.tsx
+++ testes/src/components/navbar/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <header className={style.navContainer}>
             <nav className={style.navbar}>
-                <ul className={style.linkContainer}>
+                <ul className={style.linksContainer}>
                     <li className={style.navLinks}>Home</li>
                     <li className={style.navLinks}>About</li>
                     <li className={style.navLinks}>Contact</li>
--- testes/src/pages/about/index.tsx
+++ testes/src/pages/about/index.tsx
@@ -1,5 +1,5 @@
 
-export default function page() {
+export default function About() {
     return (
         <div>
             <h1>oi</h1>
