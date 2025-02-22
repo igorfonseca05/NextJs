ef6fea7de05a2fc0343691277f47723715c0cdd8
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-02-22 08:08:34 -0300

    testes nextJs

--- testes/src/pages/index.tsx
+++ testes/src/pages/index.tsx
@@ -8,7 +8,10 @@ export default function Home() {
   return (
     <>
       <section>
-
+        <div className={styles.titleContainer}>
+          <h1 className={styles.title}>Mundo dos games</h1>
+          <p className={styles.subTitle}>Tudo sobre o universo dos games em um só lugar!</p>
+        </div>
       </section>
     </>
   );
