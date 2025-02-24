da4a87bad93cfcbba9bba3be47be502c4c1c9037
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-02-24 15:03:21 -0300

    verificando tipagem

--- testes/src/pages/index.tsx
+++ testes/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import Navbar from "../components/navbar/Navbar";
+import { useEffect, useState } from "react";
 
 
 
@@ -15,6 +16,50 @@ export default function Home() {
   // console.log(test.includes('x'))
   // console.log(test.indexOf('c'))
   // console.log(test.lastIndexOf('a'))
+
+  const nome: string = 'igor'
+  const altura: number = 185
+  const isWhite: boolean = true
+
+  const nomes: string[] = ['Caio', 'Pedro']
+  const idades: number[] = [1, 2, 3]
+
+  // Tipando objetos
+  const user: { name: string, email: string } = {
+    name: 'igor',
+    email: 'igorfonseca@gmail.com'
+  }
+
+  type Produtos = {
+    nome: string,
+    price: number
+  }
+
+  interface Produto {
+    nome: string,
+    price: number
+  }
+
+  const produto: Produto = {
+    nome: "Cadeira",
+    price: 15
+  }
+
+  // Tipando array de objetos
+
+  interface Products {
+    name: string,
+    price: number
+  }
+
+  const estoque: Products[] = [
+    { name: 'Caderno', price: 3 },
+    { name: 'Mesa', price: 10 }
+  ]
+
+  function sum(num1: number, num2: number): number {
+    return num1 + num2
+  }
 
 
 
