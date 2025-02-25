55f8d3d76d0cb013c6bb880ed5c6a726f7dae9e8
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-02-25 10:34:42 -0300

    adicionando funcionalidade de clipboard

--- my_project/src/components/tasks/Tasks.tsx
+++ my_project/src/components/tasks/Tasks.tsx
@@ -6,13 +6,20 @@ import { FiShare2 } from 'react-icons/fi'
 import Link from 'next/link';
 
 
-
 export default function Tasks({ text, publicTask, id }: { text: string, publicTask: boolean, id: string }) {
+
+    async function handleShare(id: string) {
+        await navigator.clipboard.writeText(
+            `${process.env.NEXT_PUBLIC_URL}/task/${id}`
+        )
+    }
+
+
     return (
         <article className={styles.task}>
             {publicTask && <div className={styles.tagContainer}>
                 <label className={styles.tag}>PUBLICO</label>
-                <button className={styles.shareButton}>
+                <button className={styles.shareButton} onClick={() => handleShare(id)}>
                     <FiShare2 size={22} color="#3183ff" />
                 </button>
             </div>}
