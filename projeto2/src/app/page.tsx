047cb91d9aafb68b25570a021d226652beac04d7
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-03-03 08:50:52 -0300

    using serverComponent

--- projeto2/src/app/page.tsx
+++ projeto2/src/app/page.tsx
@@ -1,10 +1,40 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 
-export default function Home() {
+
+interface DataProps {
+  id: number,
+  name: string,
+  private: boolean,
+  full_name: string,
+  owner: {
+    login: string,
+    id: number,
+    avatar_url: string
+  }
+}
+
+
+async function getData() {
+  try {
+    const res = await fetch('https://api.github.com/users/igorfonseca05/repos')
+
+    return res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export default async function Home() {
+
+  const data: DataProps[] = await getData()
+
   return (
     <div>
       <h1>Bem vindo a pagina home</h1>
+
+      <h2>Repositórios</h2>
+      {data.map((item) => (
+        <p key={item.id}><strong>Repositório: </strong>{item.name}</p>
+      ))}
     </div>
   );
 }
