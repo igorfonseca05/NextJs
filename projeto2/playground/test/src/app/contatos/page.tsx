6c60c1d19a4da040f608e08ce43607c918d0d5c1
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-03-15 09:50:20 -0300

    pasta test para treinar next

---
+++ projeto2/playground/test/src/app/contatos/page.tsx
@@ -0,0 +1,13 @@
+
+import Link from "next/link";
+
+import style from '@/app/contatos/style.module.css'
+
+export default function page() {
+    return (
+        <div>
+            <h1>Pagina de contatos</h1>
+            <p>Essa é a minha página de contatos</p>
+        </div>
+    );
+};
