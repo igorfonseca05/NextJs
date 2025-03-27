93ddf67211bbfd0169647c768bf5a566af493f83
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-03-27 09:00:11 -0300

    Rotas no next

--- projeto2/src/app/api/games/route.ts
+++ projeto2/src/app/api/games/route.ts
@@ -16,3 +16,17 @@ export async function POST(request: Request) {
 
    return NextResponse.json({message: 'Dado inserido com sucesso', games})
 }
+
+
+// Vamosa atualizar nossos dados
+export async function PUT(request: Request) {
+   const {searchParams} = new URL(request.url)
+   const index = searchParams.get('index')
+
+   const data = await request.json()
+   
+   console.log(index)
+   console.log(data)
+
+   return NextResponse.json({})
+}
