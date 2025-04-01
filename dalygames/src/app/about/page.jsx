9bd8d337a03a9a953686bd984d66c2a15a79acf4
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-04-01 16:03:56 -0300

    using tailwind to create responsive pages

---
+++ dalygames/src/app/about/page.jsx
@@ -0,0 +1,16 @@
+
+export default function About() {
+    return (
+        <div className="bg-emerald-500 max-w-screen-lg mx-auto">
+            <h1 className="text-center font-bold text-[32px]">Bem vindo</h1>
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 bg-slate-500 gap-2">
+                <div className="bg-red-300">1</div>
+                <div className="bg-sky-300">2</div>
+                <div className="bg-red-300">3</div>
+                <div className="bg-sky-300">4</div>
+                <div className="bg-red-300">5</div>
+                <div className="bg-sky-300">6</div>
+            </div>
+        </div>
+    )
+}
