e60f9faf9743a07f310d83e73580c97c60f37fbe
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-04-21 11:34:35 -0300

    estudando documentação next

--- nextdoc/src/app/components/form/Form.tsx
+++ nextdoc/src/app/components/form/Form.tsx
@@ -1,16 +1,26 @@
 
 'use client'
 
-import { salvar } from "@/app/lib/actions"
+import { salvar, getData } from "@/app/lib/actions"
+import { useActionState } from "react"
+
 
 export function Form() {
+
+    const [state, action, pending] = useActionState(salvar, { message: '' })
+
     return (
-        <form className="mt-5 bg-amber-100 p-8" action={salvar}>
-            <label htmlFor="nome">
-                <span>Nome:</span>
-                <input type="text" name="nome" className="bg-white mx-2" />
-            </label>
-            <button type="submit" className="bg-fuchsia-300 px-1">Enviar</button>
-        </form>
+        <div>
+            <form className="mt-5 bg-amber-100 p-8" action={action}>
+                <label htmlFor="nome">
+                    <span>Nome:</span>
+                    <input type="text" name="nome" className="bg-white mx-2" required={true} />
+                </label>
+                <button className="bg-fuchsia-300 px-1">Enviar</button>
+                {pending ? <p>Aguarde...</p> : state.message && <p>Dado foi adicionado</p>}
+            </form>
+
+            <button className="p-2 bg-sky-200 cursor-pointer" onClick={async () => { getData() }}>Clique aqui</button>
+        </div>
     )
 }
--- nextdoc/src/app/lib/actions.ts
+++ nextdoc/src/app/lib/actions.ts
@@ -2,10 +2,19 @@
 'use server'
 
 
-export async function salvar(formData: FormData) {
+export async function salvar(prevState: any, formData: FormData) {
     const nome = formData.get('nome')
 
     console.log(nome)
 
+    await new Promise((resolve, reject) => setTimeout(() => resolve('resolvido'), 3000))
+
     return { message: 'Adicionado com sucesso!' }
+}
+
+
+export async function getData() {
+    setTimeout(() => {
+        console.log('Clicou no botão')
+    }, 3000)
 }
