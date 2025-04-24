3b0a6e4c2e872bf30683009528a7ecf87bf187c6
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-04-24 19:47:21 -0300

    salvando usuários

--- fullsystem/src/app/page.tsx
+++ fullsystem/src/app/page.tsx
@@ -4,7 +4,10 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
-      <h1>Teste</h1>
+      {/* <h1>Teste</h1> */}
+      <section style={{ height: '100vh', width: '100vw', backgroundColor: 'blue' }}></section>
+      <section style={{ height: '100vh', width: '100vw', backgroundColor: 'yellow' }}></section>
+      <section style={{ height: '100vh', width: '100vw', backgroundColor: 'red' }}></section>
     </main>
   );
 }
--- fullsystem/src/components/header/header.tsx
+++ fullsystem/src/components/header/header.tsx
@@ -1,9 +1,41 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
+import styles from './styles.module.scss'
+import Link from "next/link"
 
 export function Header() {
+
+    const [scrollY, setScrollY] = useState(0)
+    const [change, setChange] = useState(false)
+
+    useEffect(() => {
+
+        function scrolling() {
+            if (window.scrollY > 0) {
+                setChange(true)
+            } else {
+                setChange(false)
+
+            }
+        }
+
+        window.addEventListener('scroll', scrolling)
+
+        return () => window.removeEventListener('scroll', scrolling)
+
+    }, [scrollY])
+
     return (
-        <header>
-            <nav>
-                <h1>oi</h1>
+        <header className={`${change ? `${styles.headerContainerScroll}` : `${styles.headerContainer} `}`}>
+            <div className={styles.contentLogo}>
+                <Link href={'/#home'}> Dev Motors</Link>
+            </div>
+            <nav className={styles.navContainer}>
+                <Link href={'/#home'}>Home</Link>
+                <Link href={'/#servicos'}> Serviços</Link>
+                <Link href={'/#contatos'}>Contatos</Link>
             </nav>
         </header>
     )
