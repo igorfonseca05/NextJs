a053714fb8f3756d92922883ea10c5aa4cb0ad6e
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-07-04 10:07:21 -0300

    removi projeto firebase

--- docs/db/firebase.ts
+++ docs/db/firebase.ts
@@ -1,25 +1,18 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore, doc, setDoc, addDoc, collection } from "firebase/firestore";
+// import { initializeApp } from "firebase/app";
+// import { getFirestore, doc, setDoc, addDoc, collection } from "firebase/firestore";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAz5tIHPfWy96C7xzzePMIpL8i0XHC6owQ",
-    authDomain: "testing-firebase-38808.firebaseapp.com",
-    projectId: "testing-firebase-38808",
-    storageBucket: "testing-firebase-38808.firebasestorage.app",
-    messagingSenderId: "653475614588",
-    appId: "1:653475614588:web:cdde7f527c2ecbbdb65ef9",
-    measurementId: "G-193MFSSDE8"
-};
+// Tem que criar outro projeto no firabase, vazou dado privado.
+
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app)
 
-export {
-    db,
-    setDoc,
-    addDoc,
-    collection,
-    doc
-}
+// export {
+//     db,
+//     setDoc,
+//     addDoc,
+//     collection,
+//     doc
+// }
